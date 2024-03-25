8adafc0240a22817a57f5de4e445ea09edffcab4
AnupamSingh2004 <sanupam2004@gmail.com>
2024-03-25 15:19:06 +0530

    todo-app

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -3,11 +3,7 @@ import './App.css'
 import Todos from "./todos.jsx";
 
 function App() {
-
-  
-
-
-  return (<div className={"flex items-center justify-center h-[100vh]"}>
+    return (<div className={"flex items-center justify-center h-[100vh]"}>
           <div className={""}>
               <Todos/>
           </div>
